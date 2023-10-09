92a42e035d8ff6aa6e46183069530390d62d8d74
Seungwon Choi <piepiesw@amazon.com>
2023-10-09 20:16:07 +0900

    :sparkles: Feat : add Frontend stack

---
+++ fe/src/App.js
@@ -0,0 +1,95 @@
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import reviews from "./review";
+import Card from "./card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  const handler = (id) => {
+    console.log(id);
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/http://anycompany-alb-1145759004.ap-northeast-2.elb.amazonaws.com/api/products/list"
+      )
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (response) {
+        console.log(response);
+      });
+  };
+
+  console.log(reviews);
+
+  const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background: #343434;
+  `;
+
+  const Title = styled.h1`
+    text-align: center;
+    font-weight: 900;
+    font-size: 90px;
+    color: #e2e2e2;
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+  `;
+  const InnerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const Image = styled.img`
+    width: 200px;
+  `;
+  const Subtitle = styled.div`
+    display: flex;
+    justify-content: center;
+    color: #e2e2e2;
+    font-size: 30px;
+    font-weight: 700;
+  `;
+
+  const Contents = styled.div`
+    color: #e2e2e2;
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+  `;
+  return (
+    <Wrapper>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Title>Customer Service bot</Title>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Image src="/bedrock.jpeg"></Image>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Subtitle>Amazon Bedrock</Subtitle>
+        </Row>
+        <Contents>
+          이 사이트는 Amazon Bedrock 서비스를 이용해 고객의 리뷰에 대한 답변을
+          자동화하는 Customer Service bot 데모를 구현했습니다.
+          <br />
+          이 데모는 Amazon Bedrock 서비스에서 지원하는 다양한 Foundation Model
+          중 ANTROPIC의 CLAUDE v2 모델을 사용하였습니다.
+          <br />
+        </Contents>
+        <Row className="justify-content-md-center">
+          {reviews.products.map((product) => (
+            <Card content={product.comment} index={product.index} />
+          ))}
+        </Row>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ fe/src/card.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+
+export default function Card(props) {
+  const [clicked, setClicked] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [generated, setGenerated] = useState("hi");
+  const [modify, setModify] = useState(generated);
+
+  const handleOnClick = () => {
+    setClicked(true);
+  };
+  const handleChange = (e) => {
+    setModify(e.target.value);
+    console.log("modify: " + modify);
+  };
+  const handleSubmit = () => {
+    setSubmitted(true);
+    setClicked(false);
+  };
+  return (
+    <Wrapper>
+      <Upper>
+        <Title>Review #{props.index}</Title>
+      </Upper>
+      <Content>{props.content}</Content>
+      <ButtonWrapper>
+        <Button variant="success" onClick={handleOnClick}>
+          Generate
+        </Button>
+      </ButtonWrapper>
+      {clicked && (
+        <>
+          <Reply value={modify} onChange={handleChange} />
+          <ButtonWrapper>
+            <Button variant="primary" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </ButtonWrapper>
+        </>
+      )}
+      {submitted && (
+        <>
+          <Title>Reply: </Title>
+          <Content>{modify}</Content>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  border-radius: 3rem;
+  flex-direction: column;
+  width: 80%;
+  margin: 3rem;
+`;
+const Upper = styled.div`
+  background: #ccd0d5;
+  border-radius: 2rem 2rem 0 0;
+  text-align: center;
+  height: 3rem;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+const Title = styled.p`
+  margin: 0;
+`;
+const Content = styled.div`
+  display: flex;
+  color: #e2e2e2;
+  padding: 1.5rem;
+  border: solid 1px #e2e2e2;
+`;
+const Reply = styled.textarea`
+  height: 200px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: solid 2px #1e90ff;
+  border-radius: 5px;
+  font-size: 16px;
+  resize: both;
+`;
+const ButtonWrapper = styled.div`
+  display: block;
+  margin: 1rem 0rem 1rem 1rem;
+  text-align: end;
+`;
